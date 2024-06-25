75cc377d51b6a3959b94b01c61edde4a1e8dddcb
grahametc <159605112+grahametc@users.noreply.github.com>
2024-06-25 13:30:39 -0400

    Add files via upload

--- email.js
+++ email.js
@@ -1,20 +1,29 @@
-function sendEmail(){
-   let params = {
-    name: document.getElementById("from_name").value,
-    email: document.getElementById("from_email").value,
-    phone: document.getElementById("phone_number").value,
-    message: document.getElementById("message").value
-   } 
-   emailjs.send("service_s8jlz8k", "template_vyy13df", params).then(
-    (response) => {
-      console.log('SUCCESS!', response.status, response.text);
-    },
-    (error) => {
-      console.log('FAILED...', error);
-    },
-  );
+const serviceID = "service_s8jlz8k";
+const templateID = "template_vyy13df";
+
+(function() {
+    console.log('start');
+    emailjs.init({
+      publicKey: "ShlvlNLDnKUCJtNIN"
+    });
+    console.log('end');
+})();
+console.log(document.getElementById('contact-form'));
+  window.onload = function() {
+    console.log('h');
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        console.log(event);
+        console.log(this);
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm(serviceID, templateID, this, 'ShlvlNLDnKUCJtNIN')
+            .then(() => {
+                console.log('SUCCESS!');
+                alert("Email sent successfully.")
+            }, (error) => {
+                console.log('FAILED...', error);
+                alert("Email failed to send. Please try again.")
+            });
+    });
 }
 
-function validateEmail(email){
-
-}
