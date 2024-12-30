154308a017d1758ebe3ca28166fee88cd97ab19b
grahametc <159605112+grahametc@users.noreply.github.com>
2024-12-29 22:15:16 -0500

    Add files via upload

--- email.js
+++ email.js
@@ -2,18 +2,13 @@ const serviceID = "service_s8jlz8k";
 const templateID = "template_vyy13df";
 
 (function() {
-    console.log('start');
     emailjs.init({
       publicKey: "ShlvlNLDnKUCJtNIN"
     });
-    console.log('end');
 })();
 console.log(document.getElementById('contact-form'));
   window.onload = function() {
-    console.log('h');
     document.getElementById('contact-form').addEventListener('submit', function(event) {
-        console.log(event);
-        console.log(this);
         event.preventDefault();
         // these IDs from the previous steps
         const name = document.getElementById('name').value.trim();
